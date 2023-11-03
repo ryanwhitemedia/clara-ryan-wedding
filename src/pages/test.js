35d1046941d18511510b637f8321b615be58b188
ryanwhitemedia <ryan@toniccreative.com>
2023-11-03 16:46:56 -0400

    chore: test embed

--- src/pages/test.js
+++ src/pages/test.js
@@ -3,14 +3,10 @@ import classnames from 'classnames';
 
 import styles from './index.module.scss';
 
-import Date from '@/assets/svgs/date.svg';
-
 // import Header from '@/components/Header/Header';
 import { AppContext } from '@/contexts/app-context';
 
-import Head from '../components/Head/Head';
-
-function Home({ className, token }) {
+function Test({ className, token }) {
   const { homeState } = useContext(AppContext);
 
   useEffect(() => {
@@ -36,56 +32,16 @@ function Home({ className, token }) {
 
   return (
     <main className={classnames(styles.Home, className, styles[homeState])}>
-      <Head />
-      <h1 className={classnames(styles.text)}>
-        Save the date
-        <br />
-        for the wedding of Clara + Ryan
-      </h1>
-      <Date className={classnames(styles.date)} />
-      <p>Friday, September 29, 2023</p>
-      <p>4:00pm</p>
-      <p>
-        Hosted @{' '}
-        <a href="https://eloramill.ca/" target="_blank" rel="noreferrer">
-          Elora Mill
-        </a>
-      </p>
-      <p> 77 Mill St W, Elora, ON N0B 1S0</p>
-      <div id="schedule" className={styles.schedule}>
-        <h2>Schedule</h2>
-        <div>
-          <h3>Shuttle Pickup</h3>
-          <p>3:45pm</p>
-          <p>Best Western, St. Andrews, Breadalbane Inn</p>
-        </div>
-        <div>
-          <h3>Ceremony</h3>
-          <p>4:30PM - 5:00pm</p>
-          <p>The Foundry Chapel </p>
-        </div>
-        <div>
-          <h3>Cocktails </h3>
-          <p>5:00PM - 6:30PM </p>
-          <p>Granary - Irvine Room </p>
-        </div>
-        <div>
-          <h3>Dinner reception </h3>
-          <p>6:30PM - 10:00PM </p>
-          <p>Granary - Grand Room </p>
-        </div>
-        <div>
-          <h3>Dancing </h3>
-          <p>10:00Pm - 1:30AM</p>
-          <p>Granary - Grand Room</p>
-        </div>
-        <div>
-          <h3>Shuttle Drop-Off</h3>
-          <p>10:30Pm - 1:30AM </p>
-          <p style={{ maxWidth: '450px' }}>
-            A car will be on standby outside of the venue to bring guests back to their hotels.
-          </p>
-        </div>
+      <div style={{ width: '100vw', height: '100vh' }}>
+        <iframe
+          title="FinChat"
+          name="targetFrame"
+          src={`https://widget.finchat.io/d33ce9667b684d958a5ea809cfc1a2c5`}
+          // src={`http://localhost:4000/d33ce9667b684d958a5ea809cfc1a2c5`}
+          width="100%"
+          height="100%"
+          style={{ border: 'none' }}
+        />
       </div>
     </main>
   );
@@ -105,4 +61,4 @@ function Home({ className, token }) {
 //   // return { props: { token, widgetKey }, revalidate: 5 };
 // };
 
-export default memo(Home);
+export default memo(Test);
